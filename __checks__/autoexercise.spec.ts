b3de58440808fafa818264d16d2df1f888edadaf
Conor McManamon <c.s.mcmanamon@gmail.com>
2024-12-27 15:17:57 -0700

    Aligned roles and button text to match website. Test passes.

--- __checks__/autoexercise.spec.ts
+++ __checks__/autoexercise.spec.ts
@@ -2,8 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.automationexercise.com/');
-  const consentButton = page.getByRole("button", { name: "Consent"});
-  await consentButton.click()
+  //const consentButton = page.getByRole("button", { name: "Consent"});
+  //await consentButton.click()
   await page.getByRole('link', { name: ' Products' }).click();
   const searchBar = page.getByPlaceholder('Search Product');
   expect(searchBar).toBeVisible;
@@ -11,10 +11,12 @@ test('test', async ({ page }) => {
   await addBlueTopButton.click()
   const modalHeading = page.getByRole("heading", { name: "Added!"});
   expect(modalHeading).toBeVisible;
-  await page.getByRole('link', { name: 'Continue Shopping' }).click();
+  // Updated role from link to button
+  await page.getByRole('button', { name: 'Continue Shopping' }).click();
   await page.getByRole('link', { name: 'Cart'}).click();
   await page.getByText('Proceed To Checkout').click();
-  await page.getByRole('link', { name: 'Register / Signup'}).click();
+  // Current website text shows Login as second option
+  await page.getByRole('link', { name: 'Register / Login'}).click();
   const signupHeading = page.getByRole('heading', { name: 'New User Signup!'});
   expect(signupHeading).toBeVisible;
 });
