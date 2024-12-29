3aa10df7881a29a0ff73aa9767ca23022c9ce500
Conor McManamon <c.s.mcmanamon@gmail.com>
2024-12-29 11:24:54 -0700

    Initial dynamic api tests running

---
+++ __checks__/api/api.check.ts
@@ -0,0 +1,41 @@
+import { ApiCheck, AssertionBuilder } from 'checkly/constructs'
+import dotenv from 'dotenv';
+const usersJSON = require('./userURLs.json');
+
+dotenv.config();
+
+const usersList = usersJSON.users;
+
+const token = process.env.BEARER_AUTH_TOKEN;
+
+for (const user of usersList) {
+  new ApiCheck(`check_user_${user.id}`, {
+    name: `User ${user.name}`,
+    request: {
+        method: 'GET',
+        url: user.profile_url,
+        body: JSON.stringify({
+          name: 'checkly'
+        }),
+        skipSSL: false,
+        followRedirects: true,
+        headers: [
+          {
+            key: 'Authorization',
+            value: `Bearer ${token}`,
+          }
+        ],
+        queryParameters: [
+          {
+            key: 'myParam',
+            value: 'true'
+          }
+        ],
+        assertions: [
+            AssertionBuilder.statusCode().equals(200),
+            AssertionBuilder.jsonBody('$.name').notEmpty(),
+            AssertionBuilder.jsonBody('$.name').equals(user.name),
+        ]
+      }
+  })
+}
