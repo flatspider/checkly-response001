bf31d3750074206f978be963a6b295d4b51cf3c9
Conor McManamon <c.s.mcmanamon@gmail.com>
2024-12-30 15:11:12 -0700

    Removed recovery alert

--- alert_channels/email.ts
+++ alert_channels/email.ts
@@ -3,4 +3,5 @@ import {EmailAlertChannel} from 'checkly/constructs'
 export const userEmail = new EmailAlertChannel('email-1', { 
     address: 'raccoon@checklyhq.com',
     sendFailure: true,
+    sendRecovery: false,
 })
