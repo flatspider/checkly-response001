c32c65cc68c685262987baf910f8493b1aecde79
Conor McManamon <c.s.mcmanamon@gmail.com>
2025-01-14 21:06:41 -0500

    Added type structure for users. Made API check function reuseable.

--- __checks__/api/api.check.ts
+++ __checks__/api/api.check.ts
@@ -7,31 +7,42 @@ dotenv.config();
 
 const usersList = usersJSON.users;
 
-const token = process.env.BEARER_AUTH_TOKEN;
+const bearer_token = process.env.BEARER_AUTH_TOKEN;
 
+// Defines types for the user object
+interface User {
+  id: string | number;
+  name: string;
+  profile_url: string;
+}
+
+function CreateAPIChecks(user: User, token: string | undefined ) {
+ return  new ApiCheck(`check_user_${user.id}`, {
+  name: `User ${user.name}`,
+  group: groupAPI,
+  request: {
+      method: 'GET',
+      url: user.profile_url,
+      body: JSON.stringify({
+        name: 'checkly'
+      }),
+      skipSSL: false,
+      followRedirects: true,
+      headers: [
+        {
+          key: 'Authorization',
+          value: `Bearer ${token}`,
+        }
+      ],
+      assertions: [
+          AssertionBuilder.statusCode().equals(200),
+          AssertionBuilder.jsonBody('$.name').equals(user.name),
+          AssertionBuilder.jsonBody('$.id').equals(user.id),
+      ]
+    }
+})
+}
+ 
 for (const user of usersList) {
-  new ApiCheck(`check_user_${user.id}`, {
-    name: `User ${user.name}`,
-    group: groupAPI,
-    request: {
-        method: 'GET',
-        url: user.profile_url,
-        body: JSON.stringify({
-          name: 'checkly'
-        }),
-        skipSSL: false,
-        followRedirects: true,
-        headers: [
-          {
-            key: 'Authorization',
-            value: `Bearer ${token}`,
-          }
-        ],
-        assertions: [
-            AssertionBuilder.statusCode().equals(200),
-            AssertionBuilder.jsonBody('$.name').equals(user.name),
-            AssertionBuilder.jsonBody('$.id').equals(user.id),
-        ]
-      }
-  })
+  CreateAPIChecks(user, bearer_token);
 }
